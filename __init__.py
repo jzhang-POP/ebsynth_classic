e394e00637289454644edab65fafe1d7bdc10c3c
jzhang-POP <jacques.zhang@proseonpixels.com>
2025-11-28 17:26:20 +0100

    Initial commit: ebsynth_classic node structure

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for ebsynth_classic."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """"""
+__email__ = "jacques.zhang@proseonpixels.com"
+__version__ = "0.0.1"
+
+from .src.ebsynth_classic.nodes import NODE_CLASS_MAPPINGS
+from .src.ebsynth_classic.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
