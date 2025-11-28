f0db14060b2d5e356dda22953c63f2bb683bf6a3
jzhang-POP <jacques.zhang@proseonpixels.com>
2025-11-28 17:33:16 +0100

    wip: minimum working node

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@
 __all__ = [
     "NODE_CLASS_MAPPINGS",
     "NODE_DISPLAY_NAME_MAPPINGS",
-    
 ]
 
 __author__ = """"""
@@ -12,5 +11,3 @@ __version__ = "0.0.1"
 
 from .src.ebsynth_classic.nodes import NODE_CLASS_MAPPINGS
 from .src.ebsynth_classic.nodes import NODE_DISPLAY_NAME_MAPPINGS
-
-
